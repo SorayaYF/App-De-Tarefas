e4957a33f53143740192e2b629bd20c7ce3d9a9b
SorayaYF <110129579+SorayaYF@users.noreply.github.com>
2026-05-05 19:58:21 -0300

    Criado os utilitários(Rotas e DB)

---
+++ lib/util/db.dart
@@ -0,0 +1,57 @@
+import 'package:app_de_tarefas/models/model.dart';
+import 'package:sqflite/sqflite.dart' as sqlite;
+import 'package:path/path.dart' as path;
+
+class DBUtil {
+  static Future<sqlite.Database> _getDB() async {
+    final databasePath = await sqlite.getDatabasesPath();
+    final arqBD = path.join(databasePath, 'tarefas.db');
+
+    return sqlite.openDatabase(
+      arqBD,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute('''
+          CREATE TABLE Tarefa(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            descricao TEXT NOT NULL,
+            dataPrevista TEXT NOT NULL,
+            importante INTEGER NOT NULL,
+            realizada INTEGER NOT NULL,
+            ordem INTEGER NOT NULL
+          )
+        ''');
+      },
+    );
+  }
+
+  static Future<void> insert(Model model) async {
+    final db = await _getDB();
+    model.id = await db.insert(model.runtimeType.toString(), model.toMap());
+  }
+
+  static Future<List<Map<String, dynamic>>> list(String table) async {
+    final db = await _getDB();
+    return db.query(table, orderBy: 'ordem ASC');
+  }
+
+  static Future<int> update(Model model) async {
+    final db = await _getDB();
+    return db.update(
+      model.runtimeType.toString(),
+      model.toMap(),
+      where: 'id = ?',
+      whereArgs: [model.id],
+    );
+  }
+
+  static Future<int> delete(String table, int id) async {
+    final db = await _getDB();
+    return db.delete(
+      table,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
---
+++ lib/util/rotas.dart
@@ -0,0 +1,6 @@
+class Rotas {
+  static const telaBoasVindas = '/';
+  static const telaLista = '/lista';
+  static const telaForm = '/form';
+  static const telaDetalhes = '/detalhes';
+}
