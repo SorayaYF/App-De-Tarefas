7c1e9640286b4d8a4f55873342b3ef374e90facc
ramon-heerdt <110129579+SorayaYF@users.noreply.github.com>
2026-05-08 19:57:17 -0300

    Adicionada as rotas no main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:app_de_tarefas/providers/tarefa_provider.dart';
+import 'package:app_de_tarefas/telas/tela_boas_vindas.dart';
+import 'package:app_de_tarefas/telas/tela_detalhes.dart';
+import 'package:app_de_tarefas/telas/tela_form.dart';
+import 'package:app_de_tarefas/telas/tela_lista.dart';
+import 'package:app_de_tarefas/util/rotas.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,13 +16,52 @@ class MeuApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const primaria = Color(0xFF2563EB);
+    const borda = Color(0xFFE5E7EB);
+
     return ChangeNotifierProvider(
-      create: (_) => TarefaProvider(),
+      create: (_) => TarefaProvider()..carregaTarefas(),
       child: MaterialApp(
         title: 'Tarefas',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: primaria),
+          scaffoldBackgroundColor: const Color(0xFFF8FAFC),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Color(0xFF1F2937),
+            elevation: 0,
+            scrolledUnderElevation: 0,
+            shape: Border(bottom: BorderSide(color: borda)),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            filled: true,
+            fillColor: Colors.white,
+            isDense: true,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: borda),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: borda),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: primaria, width: 1.5),
+            ),
+          ),
         ),
+        initialRoute: Rotas.telaBoasVindas,
+        routes: {
+          Rotas.telaBoasVindas: (_) => const TelaBoasVindas(),
+          Rotas.telaLista: (_) => const TelaLista(),
+          Rotas.telaForm: (_) => const TelaForm(),
+          Rotas.telaDetalhes: (_) => const TelaDetalhes(),
+        },
       ),
     );
   }
